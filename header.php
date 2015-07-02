a2e7acc9aa97de60697f805bc403e5cb7126ddf4
A M <mcblood1@gmail.com>
2015-07-01 20:31:23 -0700

    Added css framework and added temporary content to index page

--- header.php
+++ header.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
 		<title>Crypto Sports League</title>
+		<meta name="description" content="Crypto Sports League is an eSports league that's primary currency is Bitcoin.">
+		<meta name="author" content="">
 
-		<link rel="stylesheet" type="text/css" href="index.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="css/skeleton.css">
+		<link rel="stylesheet" type="text/css" href="css/index.css">
 	</head>
 	<body>
 
--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 	include 'header.php';
 ?>
 
-
+<div class="container">
+    <div class="row">
+      <div class="one-half column" style="margin-top: 25%">
+        <h4>The Site is in developement</h4>
+        <p>This site is currently being worked on. Come back soon for updates.</p>
+      </div>
+    </div>
+</div>
 
 <?php
 	include 'footer.php';
