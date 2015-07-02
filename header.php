8e52d4d6cdcad8e239b8a6f03149f29bf4e779f0
A M <mcblood1@gmail.com>
2015-07-02 04:08:34 -0700

    Changed Livestream to Blog

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 					<ul class="navbar-list">
 						<li class="navbar-item"><a class="navbar-link" href="index.php">Home</a></li>
 						<li class="navbar-item"><a class="navbar-link" href="#">Tournament Status</a></li>
-						<li class="navbar-item"><a class="navbar-link" href="#">Livestream</a></li>
+						<li class="navbar-item"><a class="navbar-link" href="#">Blog</a></li>
 						<li class="navbar-item"><a class="navbar-link" href="#">About Us</a></li>
 					</ul>
 					<ul class="navbar-list u-pull-right">
